030265bde67540495f5d527e80dbe920a4eb580d
Christopher Marston <christopher.marston@bluestaq.com>
2022-07-01 15:57:20 -0600

    Completed Chapter 7.

--- chapter_7/submit_events.js
+++ chapter_7/submit_events.js
@@ -7,13 +7,14 @@ const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
 // const username = document.querySelector('#username'); // This is one way to get the username field form the form
 
+const usernamePattern = /^[a-zA-Z]{6,10}$/;
+
 form.addEventListener('submit', e => {
     e.preventDefault(); // This prevents the default behavior of submitting a form. This allows us to define our own behavior from scratch.
     // console.log(form.username.value); // This gets the value from the username field in the form
 
     // Input validation
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,10}$/;
 
     if (usernamePattern.test(username)) {
         // Valid input
@@ -21,6 +22,21 @@ form.addEventListener('submit', e => {
     } else {
         // Invalid input
         feedback.textContent = 'Username is invalid';
+    }
+});
+
+// Live feedback - marks the input as valid/invalid as the user is typing
+
+// This listens for the event that fires when a key is released.
+// Not sure why, but this isn't working...It requires that I click off of the texbox before it applies the class.
+form.username.addEventListener('onkeyup', e => {
+    // console.log(e.target.value, form.username.value);
+    if (usernamePattern.test(e.target.value)) { 
+        // console.log('Valid');
+        form.username.setAttribute('class', 'valid_input'); // The setAttribute() method completely overrides any classes already applied to the target element. That is, it removes any existing classes and adds the new one.
+    } else {
+        // console.log('Invalid');
+        form.username.setAttribute('class', 'invalid_input');
     }
 });
 
